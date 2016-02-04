0edf28c3a2432c0d44fc05eeed932fa05ebd15c5
bondan23 <bondan.ekoprasetyo@gmail.com>
2016-02-04 16:52:27 +0700

    Iseng Rubah

--- index.php
+++ index.php
@@ -9,10 +9,13 @@ use myproject\apps as b;
 
 use myproject\bondan\MyConnect;
 
-$conn = new a\Connection();
+//$conn = new a\Connection();
 echo "<br>";
 $conn2 = new MyConnect();
 echo "<br>";
 $conn2->connect("A","B","C");
 echo "<br>";
 $conn3 = new b\Connection();
+
+
+//TAMBAH COMMAND
